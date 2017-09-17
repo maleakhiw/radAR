e3b71b89dc5c873108f9e1ba47f5a44057aea896
Sierra Nightingale <me@nightieti.me>
2017-09-17 15:04:10 +1000

    finalize user detail and tracking group detail GUI

---
+++ radAR/app/src/main/java/com/oxygen/radar/ChatActivity.java
@@ -0,0 +1,8 @@
+package com.oxygen.radar;
+
+/**
+ * Created by Fadhil Anshar on 17-Sep-17.
+ */
+
+public class ChatActivity {
+}
---
+++ radAR/app/src/main/java/com/oxygen/radar/TrackingGroupDetailsPageActivity.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Fadhil Anshar on 16-Sep-17.
+ */
+
+public class TrackingGroupDetailsPageActivity {
+}
