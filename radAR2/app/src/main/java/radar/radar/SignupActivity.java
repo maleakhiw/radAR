9d743ad778a9f7dca18b8c070493dbfef02befd0
maleakhiw <keystorm7@gmail.com>
2017-09-19 18:02:35 +1000

    Finish signup activity

---
+++ radAR2/app/src/main/java/radar/radar/SignupActivity.java
@@ -0,0 +1,94 @@
+package radar.radar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import radar.radar.Models.Requests.SignUpRequest;
+import radar.radar.Models.Responses.AuthResponse;
+import radar.radar.Services.AuthApi;
+import radar.radar.Services.AuthService;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class SignupActivity extends AppCompatActivity {
+    private EditText firstName;
+    private EditText lastName;
+    private EditText email;
+    private EditText username;
+    private EditText password;
+    private Button signup;
+
+    private AuthService authService;  // service for making requests to our API
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        // Setup UI
+        setupUI();
+
+        // get a Retrofit instance (this is also called the Builder pattern)
+        Retrofit retrofit = new Retrofit.Builder()
+                                        .baseUrl("http://35.185.35.117/api/")
+                                        .addConverterFactory(GsonConverterFactory.create())
+                                        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                                        .build();
+
+        // make a new AuthApi using our current Retrofit *instance*
+        AuthApi authApi = retrofit.create(AuthApi.class);
+
+        // get an instance of the service. Ideally we want to use a Factory or use DI (dependency injection) to a class as a dependency
+        // so we can mock the service instead of locking us to use the real service
+        authService = new AuthService(authApi, this);
+
+        // When the user sign up, we want to pass everything to the server and database
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SignUpRequest signUpRequest = new SignUpRequest(firstName.getText().toString(),
+                        lastName.getText().toString(), email.getText().toString(), username.getText().toString(),
+                        "", password.getText().toString(), "fakeDeviceID");
+
+                authService.signUp(signUpRequest).subscribe(new Observer<AuthResponse>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(AuthResponse authResponse) {
+                        // Jump to new activity
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Toast.makeText(getApplicationContext(), "Error occurred", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+            }
+        });
+    }
+
+    /** Setting up the User Interface */
+    public void setupUI() {
+        username = (EditText) findViewById(R.id.username);
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        signup = (Button) findViewById(R.id.signup);
+        firstName = (EditText) findViewById(R.id.firstName);
+        lastName = (EditText) findViewById(R.id.lastName);
+    }
+}
