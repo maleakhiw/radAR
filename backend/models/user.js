f9309e8022ab2f6dc45f454f7a7af961e2e1f1ae
krusli <rusli.kenneth@gmail.com>
2017-09-19 15:56:53 +1000

    Update models to make userID and email unique

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-  userID: Number,
+  userID: {type: Number, unique: true},
   username: String,
   firstName: String,
   lastName: String,
-  email: String,
+  email: {type: String, unique: true},
   profilePicture: String,
   profileDesc: String,
   friends: [Number],
