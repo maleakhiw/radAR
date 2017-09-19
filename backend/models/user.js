a5f27d9203e0e63ba73d6bd0678bd3fe47da787f
krusli <rusli.kenneth@gmail.com>
2017-09-19 16:09:24 +1000

    Typo

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const uniqueValidator = require('mongoose-unique-validation')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const userSchema = new Schema({
   userID: {type: Number, unique: true},
