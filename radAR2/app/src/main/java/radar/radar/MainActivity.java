fe337ee865b50282bf56bd1267506e798aed7776
maleakhiw <keystorm7@gmail.com>
2017-09-19 17:36:28 +1000

    Signup form and attaching event listener, request object

--- radAR2/app/src/main/java/radar/radar/MainActivity.java
+++ radAR2/app/src/main/java/radar/radar/MainActivity.java
@@ -2,10 +2,16 @@ package radar.radar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import radar.radar.Models.Requests.SignUpRequest;
+
 public class MainActivity extends AppCompatActivity {
+    private EditText firstName;
+    private EditText lastName;
+    private EditText email;
     private EditText username;
     private EditText password;
     private Button signup;
@@ -17,6 +23,16 @@ public class MainActivity extends AppCompatActivity {
 
         // Setup UI
         setupUI();
+
+        // When the user sign up, we want to pass everything to the server and database
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SignUpRequest signUpRequest = new SignUpRequest(firstName.getText().toString(),
+                        lastName.getText().toString(), email.getText().toString(), username.getText().toString(),
+                        null, password.getText().toString(), null);
+            }
+        });
     }
 
     /** Setting up the User Interface */
@@ -24,5 +40,7 @@ public class MainActivity extends AppCompatActivity {
         username = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
         signup = (Button) findViewById(R.id.signup);
+        firstName = (EditText) findViewById(R.id.firstName);
+        lastName = (EditText) findViewById(R.id.lastName);
     }
 }
