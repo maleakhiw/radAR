0abeb578d67e33d4360eabc757d7affd4fd1f0e1
maleakhiw <keystorm7@gmail.com>
2017-09-19 17:16:54 +1000

    Setting up the signup interface and event listener

--- radAR2/app/src/main/java/radar/radar/MainActivity.java
+++ radAR2/app/src/main/java/radar/radar/MainActivity.java
@@ -2,12 +2,27 @@ package radar.radar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText username;
+    private EditText password;
+    private Button signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Setup UI
+        setupUI();
+    }
+
+    /** Setting up the User Interface */
+    public void setupUI() {
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        signup = (Button) findViewById(R.id.signup);
     }
 }
