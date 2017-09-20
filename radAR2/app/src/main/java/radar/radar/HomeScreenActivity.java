a9735c12f677620bebb8ade81576e03a69a7cfbb
maleakhiw <keystorm7@gmail.com>
2017-09-20 11:34:53 +1000

    Fixed the navigation drawer

--- radAR2/app/src/main/java/radar/radar/HomeScreenActivity.java
+++ radAR2/app/src/main/java/radar/radar/HomeScreenActivity.java
@@ -1,5 +1,7 @@
 package radar.radar;
 
+import android.content.ClipData;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class HomeScreenActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -80,17 +83,21 @@ public class HomeScreenActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_maps) {
+            //TODO: Go to maps
+        } else if (id == R.id.nav_chats) {
+            //TODO: Go to chats
+        } else if (id == R.id.nav_friends) {
+            Intent intent = new Intent(this, FriendsActivity.class);
+            startActivity(intent);
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_logout) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_tracking_groups) {
 
         }
 
