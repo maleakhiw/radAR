a193ee0922df7a2daf26b0ba7dcfbd375a1a490e
Nightingale <me@nightieti.me>
2017-09-20 14:50:51 +1000

    camera source

---
+++ radAR2/app/src/main/java/radar/radar/ARActivity.java
@@ -0,0 +1,13 @@
+package radar.radar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ARActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ar);
+    }
+}
