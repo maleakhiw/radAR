e251575a8c1697474c19fd29f13eb66b25f35c0b
krusli <rusli.kenneth@gmail.com>
2017-09-28 20:19:33 +1000

    Camera view in ARActivity2 working

--- radAR2/app/src/main/java/radar/radar/ARActivity2.java
+++ radAR2/app/src/main/java/radar/radar/ARActivity2.java
@@ -1,20 +1,53 @@
 package radar.radar;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.TotalCaptureResult;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Surface;
+import android.view.TextureView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import org.reactivestreams.Subscriber;
 
-import io.reactivex.internal.operators.observable.ObservableInterval;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 import radar.radar.Models.UserLocation;
 import radar.radar.Presenters.ARPresenter;
 import radar.radar.Views.ARAnnotation;
 import radar.radar.Views.ARView;
+
+class CameraData {
+    String cameraID;
+    CameraCharacteristics cameraCharacteristics;
+
+    public CameraData(String cameraID, CameraCharacteristics cameraCharacteristics) {
+        this.cameraID = cameraID;
+        this.cameraCharacteristics = cameraCharacteristics;
+    }
+}
 
 public class ARActivity2 extends AppCompatActivity implements ARView {
 
@@ -24,6 +57,136 @@ public class ARActivity2 extends AppCompatActivity implements ARView {
     LayoutInflater inflater;
 
     ARPresenter presenter;
+
+    // camera code adapted from https://willowtreeapps.com/ideas/camera2-and-you-leveraging-android-lollipops-new-camera/
+
+    int REQUEST_FOR_CAMERA = 1;
+
+    Observable<Surface> getSurfaceFromTextureView(TextureView previewView) {
+        return Observable.create(observableEmitter -> {
+            previewView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
+                @Override
+                public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int width, int height) {
+                    observableEmitter.onNext(new Surface(surfaceTexture));
+                }
+
+                @Override
+                public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int width, int height) {
+
+                }
+
+                @Override
+                public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+                    return false;
+                }
+
+                @Override
+                public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+
+                }
+            });
+        });
+    }
+
+    Observable<CameraData> getCameraData(@NonNull CameraManager cameraManager) {
+        return Observable.create(observableEmitter -> {
+            String[] cameraIDs = cameraManager.getCameraIdList();   // first camera
+            if (cameraIDs.length > 0) {
+                // take the 1st camera. Assume to be rear camera. TODO remove assumption, allow selection
+                String cameraID = cameraIDs[0];
+                CameraCharacteristics cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraID);
+                // NOTE cameraCharacteristics contains camera info (lens, sensor)
+                // use to calculate FoV
+
+                // check for permissions
+                int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+                if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+                    // granted, open the camera and get the CameraDevice
+                    // then create a capture session
+                    observableEmitter.onNext(new CameraData(cameraID, cameraCharacteristics));
+
+                } else {    // PERMISSION_DENIED
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_FOR_CAMERA);
+                }
+            }
+        });
+    }
+
+    Observable<CameraDevice> getCameraDevice(@NonNull CameraManager cameraManager, @NonNull String cameraID) {
+        return Observable.create(observableEmitter -> {
+            int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+            if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+                cameraManager.openCamera(cameraID, new CameraDevice.StateCallback() {
+                    @Override
+                    public void onOpened(@NonNull CameraDevice cameraDevice) {
+                        observableEmitter.onNext(cameraDevice);
+
+                    }
+
+                    @Override
+                    public void onDisconnected(@NonNull CameraDevice cameraDevice) {
+
+                    }
+
+                    @Override
+                    public void onError(@NonNull CameraDevice cameraDevice, int i) {
+
+                    }
+                }, new Handler(message -> false));
+
+            }
+        });
+    }
+
+    Observable<Boolean> createCaptureSession(@NonNull Surface previewSurface, @NonNull CameraDevice cameraDevice) {
+       return Observable.create(observableEmitter -> {
+           // create a new CaptureSession
+           List<Surface> surfaces = Arrays.asList(previewSurface); // NOTE if we have other surfaces, include them too
+           cameraDevice.createCaptureSession(surfaces, new CameraCaptureSession.StateCallback() {
+               @Override
+               public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
+                   // we now have a capture session
+
+                   // now we have full control over image capture
+                   CaptureRequest request;
+                   try {
+                       CaptureRequest.Builder request2 = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+                       request2.addTarget(previewSurface);
+                       request = request2.build();
+
+                       List<CaptureRequest> requestList = new ArrayList<>();
+                       requestList.add(request);
+
+                       // set capture options here
+
+                       // set the session to continuously get data from the camera
+                       cameraCaptureSession.setRepeatingBurst(requestList, new CameraCaptureSession.CaptureCallback() {
+                           @Override
+                           public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
+                               super.onCaptureCompleted(session, request, result);
+                           }
+                       }, new Handler(message -> false));
+
+                       observableEmitter.onNext(true);
+                   } catch (CameraAccessException e) {
+                       observableEmitter.onError(e);
+                   }
+
+               }
+
+               @Override
+               public void onConfigureFailed(@NonNull CameraCaptureSession cameraCaptureSession) {
+
+               }
+           }, new Handler(message -> {
+               // TODO log
+               return false;
+           }));
+       });
+    }
+
+    TextureView previewView;
+    Surface mSurface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +204,75 @@ public class ARActivity2 extends AppCompatActivity implements ARView {
         // stub: poll for data from server
 //        ObservableInterval.
 
+        previewView = findViewById(R.id.AR2_texture_view);
+
+        setupCameraPrewiew();
+
+    }
+
+    void setupCameraPrewiew() {
+        // Using Camera2 as a camera preview API
+        // 1. Get a Surface to draw on from the TextureView, then
+        // 2. Fetch camera data, then
+        // 3. Get capture device, then
+        // 4. Create a new capture session
+
+        // fetch camera data
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+
+        getSurfaceFromTextureView(previewView)
+                .switchMap((surface) -> {   // similar to .then() chaining in JS
+                    System.out.println(surface);
+                    mSurface = surface;
+                    return getCameraData(cameraManager);
+                })
+                .switchMap((cameraData) -> getCameraDevice(cameraManager, cameraData.cameraID))
+                .switchMap((cameraDevice -> createCaptureSession(mSurface, cameraDevice)))
+                .subscribe(new Observer<Boolean>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Boolean aBoolean) {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.w("setupCameraPreview", "Error occurred");
+                        System.out.println(e);
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+    }
+
+
+
+
+
+
+    // called when permissions request completed (event from Activity)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == REQUEST_FOR_CAMERA) {
+            // if request is cancelled, the result arrays are empty
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // permission was granted!
+                setupCameraPrewiew();
+
+            } else {
+                // permission denied!
+                // TODO show TextView in activity, say that permission was not granted
+            }
+        }
     }
 
     /**
