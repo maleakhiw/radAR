f9c44f45cff1f1f152427783e5edb34160db529d
maleakhiw <keystorm7@gmail.com>
2017-10-01 15:00:37 +1100

    Adding layout for the chat list

--- radAR2/app/src/main/java/radar/radar/ChatListActivity.java
+++ radAR2/app/src/main/java/radar/radar/ChatListActivity.java
@@ -3,7 +3,10 @@ package radar.radar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import radar.radar.Services.ChatService;
+
 public class ChatListActivity extends AppCompatActivity {
+    private ChatService chatService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
