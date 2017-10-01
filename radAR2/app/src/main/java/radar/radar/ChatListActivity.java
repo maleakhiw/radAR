5f5fc773295b5e42ff449a7de1349e14ea5d8ee1
maleakhiw <keystorm7@gmail.com>
2017-10-01 14:52:52 +1100

    Add android row for chat (xml file)

---
+++ radAR2/app/src/main/java/radar/radar/ChatListActivity.java
@@ -0,0 +1,13 @@
+package radar.radar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChatListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_list);
+    }
+}
