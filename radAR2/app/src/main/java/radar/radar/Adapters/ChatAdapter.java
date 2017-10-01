fa5dd2071b7752739fdf36fc862ebb8cbd64d875
maleakhiw <keystorm7@gmail.com>
2017-10-01 14:58:52 +1100

    Finish the chat adapter

---
+++ radAR2/app/src/main/java/radar/radar/Adapters/ChatAdapter.java
@@ -0,0 +1,78 @@
+package radar.radar.Adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import radar.radar.Models.Chat;
+import radar.radar.R;
+import radar.radar.UserDetailActivity;
+
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
+
+    ArrayList<Chat> chats;
+    Context context;
+
+    public ChatAdapter(Context context, ArrayList<Chat> chats) {
+        this.context = context;
+        this.chats = chats;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // inflate custom layout
+        View titleView = inflater.inflate(R.layout.row_chat, parent, false);
+
+        // return a new VH instance
+        return new ViewHolder(titleView);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Chat chat = chats.get(position);
+
+        // load stuff
+        holder.chatName.setText(chat.name);
+    }
+
+    @Override
+    public int getItemCount() {
+        return chats.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView chatName;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            chatName = itemView.findViewById(R.id.row_chat_name);
+
+            // Setup on click listener on the view
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // From the displayed friend list send information
+                    Chat chat = chats.get(getAdapterPosition());
+
+                    Toast.makeText(context, "Clicked", Toast.LENGTH_LONG).show();
+                    //Intent intent = new Intent(context, UserDetailActivity.class);
+                    //intent.putExtra("user", user);
+                    //context.startActivity(intent);
+
+                }
+            });
+        }
+    }
+}
