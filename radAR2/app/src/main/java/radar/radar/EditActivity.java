5caaf54ffd1d10c5ff40f8ff2efd604ce412e6d8
maleakhiw <keystorm7@gmail.com>
2017-10-10 20:41:12 +1100

    Setting up for user so that they can choose image from the gallery as their picture

--- radAR2/app/src/main/java/radar/radar/EditActivity.java
+++ radAR2/app/src/main/java/radar/radar/EditActivity.java
@@ -1,9 +1,17 @@
 package radar.radar;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import radar.radar.Services.ResourcesApi;
 import radar.radar.Services.ResourcesService;
@@ -15,6 +23,9 @@ public class EditActivity extends AppCompatActivity {
     private ImageView preview;
     private Button pickImage;
     private Button upload;
+    String[] mediaColumns = { MediaStore.Video.Media._ID };
+    ProgressDialog progressDialog;
+    String mediaPath;
 
     private ResourcesService resourcesService;
 
@@ -25,6 +36,10 @@ public class EditActivity extends AppCompatActivity {
 
         // Setup UI
         setupUI();
+
+        // Setup progress dialog
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Uploading...");
 
         // Initiate retrofit object
         Retrofit retrofit = new Retrofit.Builder()
@@ -37,7 +52,54 @@ public class EditActivity extends AppCompatActivity {
         ResourcesApi resourcesApi = retrofit.create(ResourcesApi.class);
         resourcesService = new ResourcesService(resourcesApi, this);
 
-        
+        // Setup onclick listener for button
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadFile();
+            }
+        });
+
+        // Setup onclick listener for picking image
+        pickImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, 0);
+            }
+        });
+
+    }
+
+    /** Method for getting the activity result since we expect result */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            // When an Image is picked
+            if (requestCode == 0 && resultCode == RESULT_OK && null != data) {
+
+                // Get the Image from data
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+                Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                assert cursor != null;
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                mediaPath = cursor.getString(columnIndex);
+                // Set the Image in ImageView for Previewing the Media
+                preview.setImageBitmap(BitmapFactory.decodeFile(mediaPath));
+                cursor.close();
+
+            } else {
+                Toast.makeText(this, "You haven't picked Image/Video", Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
+        }
+
     }
 
     /** Setup UI */
@@ -46,6 +108,9 @@ public class EditActivity extends AppCompatActivity {
         pickImage = findViewById(R.id.pick_img);
         upload = findViewById(R.id.upload);
     }
+
+    /** Use for uploading file */
+    
 
 
 }
