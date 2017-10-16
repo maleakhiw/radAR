7b1684f46542184e44f36f52789795b563a0e5b6
krusli <rusli.kenneth@gmail.com>
2017-10-16 22:02:30 +1100

    Remove legacy from root dir

--- radAR/app/src/main/java/com/oxygen/radar/ChatActivity.java
+++
@@ -1,8 +0,0 @@
-package com.oxygen.radar;
-
-/**
- * Created by Fadhil Anshar on 17-Sep-17.
- */
-
-public class ChatActivity {
-}
--- radAR/app/src/main/java/com/oxygen/radar/Models/Responses/AddFriendResponse.java
+++
@@ -1,9 +0,0 @@
-package com.oxygen.radar.Models.Responses;
-
-/**
- * Created by kenneth on 18/9/17.
- */
-
-public class AddFriendResponse extends Status {
-    public int requestID;
-}
