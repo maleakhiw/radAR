9da5c6bbbd5946f718f8295a3f3901207f98b70d
krusli <rusli.kenneth@gmail.com>
2017-10-17 18:04:04 +1100

    Profile pictures caching

--- radAR2/app/src/main/java/radar/radar/Services/ResourcesService.java
+++ radAR2/app/src/main/java/radar/radar/Services/ResourcesService.java
@@ -10,8 +10,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -43,7 +41,7 @@ public class ResourcesService {
     // http://www.codexpedia.com/android/retrofit-2-and-rxjava-for-file-downloading-in-android/
     // adapted for RxJava2
 
-    private Observable<File> saveToDiskRxWithCache(final Response<ResponseBody> response, String fileID) {
+    private Observable<File> saveToDiskRxAndCache(final Response<ResponseBody> response, String fileID) {
         return saveToDiskRx(response).map(file -> {
             // copy to FileOutputStream
             FileOutputStream fos = context.openFileOutput(fileID, Context.MODE_PRIVATE);
@@ -142,31 +140,31 @@ public class ResourcesService {
         // Restore preferences
         SharedPreferences prefs = context.getSharedPreferences("radar.radar", Context.MODE_PRIVATE);
 
-        if (prefs.contains(resourceID)) {  // TODO for now false
+        if (prefs.contains(resourceID)) {
+            System.out.println("File has been cached");
             try {
                 FileInputStream fis = context.openFileInput(resourceID);
 
                 File tempFile = File.createTempFile("tmp", "suffix");
                 FileOutputStream tempFileOutputStream = new FileOutputStream(tempFile);
                 IOUtils.copy(fis, tempFileOutputStream);
-                
+
                 return Observable.just(tempFile);
+
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
-                prefs.edit().putBoolean("resourceID", true).commit();
-                // pass
             } catch (IOException e) {
                 e.printStackTrace();
-                prefs.edit().putBoolean("resourceID", true).commit();
             }
-        } else {
-            prefs.edit().putBoolean("resourceID", true).commit();
         }
-
         int userID = AuthService.getUserID(context);
         String token = AuthService.getToken(context);
         return resourcesApi.getResource(userID, resourceID, token)
-                .flatMap(responseBodyResponse -> saveToDiskRxWithCache(responseBodyResponse, resourceID))
+                .flatMap(responseBodyResponse -> saveToDiskRxAndCache(responseBodyResponse, resourceID))
+                .map(file -> {
+                    prefs.edit().putBoolean(resourceID, true).commit();
+                    return file;
+                })
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
