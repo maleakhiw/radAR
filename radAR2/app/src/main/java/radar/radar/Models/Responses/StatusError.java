dc3af41d5a1f171dfa4e80a441ff072637b31f89
maleakhiw <keystorm7@gmail.com>
2017-10-21 20:13:54 +1100

    add more comments to response data model

--- radAR2/app/src/main/java/radar/radar/Models/Responses/StatusError.java
+++ radAR2/app/src/main/java/radar/radar/Models/Responses/StatusError.java
@@ -1,9 +1,15 @@
 package radar.radar.Models.Responses;
 
+/**
+ * Another data model for status representing error on response
+ */
 public class StatusError {
     public String reason;
     public int errorCode;
 
+    /**
+     * Constructor
+     */
     public StatusError(String reason, int errorCode) {
         this.reason = reason;
         this.errorCode = errorCode;
